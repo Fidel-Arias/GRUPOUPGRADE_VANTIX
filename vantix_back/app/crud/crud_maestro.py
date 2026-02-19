4e5afafa417292bd374b6d8de54b0ea1169ecb4c
fidel-arias <fr06arias16@gmail.com>
2026-02-19 12:24:56 -0500

    refactor: correccion en la importacion de los models en crud_aestro.py

--- vantix_back/app/crud/crud_maestro.py
+++ vantix_back/app/crud/crud_maestro.py
@@ -1,10 +1,10 @@
 from typing import Optional
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
-from app.models.maestro import MaestroEntidades
-from app.schemas.maestro import EntidadCreate, EntidadBase
+from app.models.clientes import MaestroEntidades
+from app.schemas.maestro import MaestroCreate, MaestroUpdate, MaestroResponse
 
-class CRUDMaestro(CRUDBase[MaestroEntidades, EntidadCreate, EntidadBase]):
+class CRUDMaestro(CRUDBase[MaestroEntidades, MaestroCreate, MaestroUpdate]):
     
     def get_by_ruc(self, db: Session, *, ruc: str) -> Optional[MaestroEntidades]:
         return db.query(MaestroEntidades).filter(MaestroEntidades.ruc == ruc).first()
