b15a011d530ffccd4d302e726fc36ea5844938e6
fidel-arias <fr06arias16@gmail.com>
2026-02-19 13:18:05 -0500

    refactor: cambio del parametro limit a 10

--- vantix_back/app/api/v1/controller/cartera.py
+++ vantix_back/app/api/v1/controller/cartera.py
@@ -11,7 +11,7 @@ router = APIRouter()
 def listar_cartera_oficial(
     db: Session = Depends(deps.get_db),
     skip: int = 0, 
-    limit: int = 1000
+    limit: int = 10
 ):
     """
     Devuelve la lista oficial de clientes.
